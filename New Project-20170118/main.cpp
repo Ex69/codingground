4a57f74a5147381b7dde6cf66bdcc559f5600823
Ex69 <toms.skyrim@gmail.com>
2017-01-18 13:32:49 +0200

    Created by Coding Ground Client

---
+++ New Project-20170118/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  
+  int x;
+  int y;
+  int summa;
+  
+  x=23;
+  y=2;
+  
+  summa = x + y;
+  
+  cout << "SKAITLA " << x <<" UN "<< y <<" SUMMA IR "<< summa << endl;
+  
+  
+  
+   cout << "Toms Barkans" << endl; 
+   cout << "Sveika Pasaule" << endl;
+   return 0;
+}
+
